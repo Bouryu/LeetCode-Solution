53f4ccf492d288412d4fa20217a3b7ff79bab4cd
Gary Shiu <garyshiu21@gmail.com>
2022-01-18 00:12:57 +0800

    solve 290

---
+++ 290_Word_pattern/solution.py
@@ -0,0 +1,6 @@
+def wordPattern(pattern: str, s: str) -> bool:
+    words = s.split(" ")
+    if len(words) != len(pattern):
+        return False
+    return len(set(pattern)) == len(set(words)) == len(set(zip(pattern,words)))
+    
